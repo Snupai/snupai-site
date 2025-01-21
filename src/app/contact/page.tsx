12ecbcd6ccacc1551b16e09cc87b1f298e9a7c4a
Snupai~ <nya@snupai.me>
2025-01-21 18:12:39 +0100

    Update import path in Contact page to use absolute path for improved module resolution

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,5 @@
 import { type Metadata } from "next";
-import ContactForm from "~/components/ContactForm";
+import ContactForm from "@/components/ContactForm";
 
 export const metadata: Metadata = {
   title: "Contact Snupai",
